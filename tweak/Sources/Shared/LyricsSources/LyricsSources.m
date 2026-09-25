e2d6fabf4d6499c8270e5eb7951bc02f51755c1d
dayeos2015-cpu <dayeos2015@gmail.com>
2026-09-24 19:59:19 -0500

    Add import for SpicyLyrics.m

--- tweak/Sources/Shared/LyricsSources/LyricsSources.m
+++ tweak/Sources/Shared/LyricsSources/LyricsSources.m
@@ -9,6 +9,7 @@
 // when those above it had nothing timed, and then only for what they lacked.
 #import "Core/SGCore.h"
 #import "LyricsSources.h"
+#import "SpicyLyrics.m"
 #import "Shared/Lyrics/Lyrics.h"
 #import "Headers/SPTPlayer.h"
 #import <stdatomic.h>
